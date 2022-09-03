5d22cbbacb8b0f0afcb39f2e5a31eb622cea4194
Sandeep <kumar0020@gmail.com>
2022-09-03 20:17:30 +0530

    corrected path for the orient.py code. Later remove the path dependency

--- generate_configuration/specific-config.py
+++ generate_configuration/specific-config.py
@@ -43,14 +43,14 @@ fp.close()
 # the plane normal is along z-axis 
 
 tmpconfig='tmpconfig'
-input_in="python3 ./orient.py "+ ip["filename"] + " -p " + str(ip["atom1"]) +" "+ str(ip["atom2"]) +" " + str(ip["atom3"]) + " -tc > " + tmpconfig +"0.xyz"
+input_in="python3 ../orient.py "+ ip["filename"] + " -p " + str(ip["atom1"]) +" "+ str(ip["atom2"]) +" " + str(ip["atom3"]) + " -tc > " + tmpconfig +"0.xyz"
 
 os.system(input_in)
 
 
 for i in range(1,ip["size"]):
 
-  input_in="python3 ./orient.py "+ tmpconfig+str(i-1)+".xyz" + " -tz " + str(ip["tz"]) + " -tx " + str(ip["tx"])  + " -ty " + str(ip["ty"])   + " -rz " + str(ip["twist"]) +"  > tmpconfig"+str(i)+".xyz" 
+  input_in="python3 ../orient.py "+ tmpconfig+str(i-1)+".xyz" + " -tz " + str(ip["tz"]) + " -tx " + str(ip["tx"])  + " -ty " + str(ip["ty"])   + " -rz " + str(ip["twist"]) +"  > tmpconfig"+str(i)+".xyz" 
 
   os.system(input_in)
  
